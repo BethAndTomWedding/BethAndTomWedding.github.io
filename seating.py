d9594275f7ea8792ff78cb15084f153b51fc9c9d
Tom McKeesick <tmck01@gmail.com>
2025-01-14 21:55:16 +1100

    embed svg as object & img
    this works with alignment and fonts :tada:

--- seating.py
+++ seating.py
@@ -2,9 +2,11 @@
 import csv, json, sys
 from bs4 import BeautifulSoup
 
-ifpath, ofpath = sys.argv[1], sys.argv[2]
+svg_ifpath, svg_ofpath, html_ofpath = sys.argv[1:]
 
-print(f'{ifpath=} > {ofpath=}')
+print(f'{svg_ifpath=} > {svg_ofpath=}')
+
+# read the CSV of invitees ----------------------
 
 tables_to_apply = {'1': {'L': [], 'R': []}, '2': {'L': [], 'R': []},'3': {'L': [], 'R': []},}
 with open('invitees.csv') as istream:
@@ -12,14 +14,13 @@ with open('invitees.csv') as istream:
     for row in reader:
         print(row)
         for key, person_name in row.items():
-            # if not person_name:
-            #     continue
             table_n, side = key.split(' ')
             tables_to_apply[table_n][side].append(person_name)
 print(json.dumps(tables_to_apply, indent=2))
 
-soup = BeautifulSoup(open(ifpath).read(), features='xml')
+# read the template SVG --------------------------
 
+soup = BeautifulSoup(open(svg_ifpath).read(), features='xml')
 print(soup.prettify())
 
 tables_in_svg = {
@@ -36,20 +37,39 @@ tables_in_svg = {
         'R': soup.find('g', {'id': 'table3RHS'}),
     },
 }
-
 print(tables_in_svg)
 
-# take the seats from the "tables_to_apply" and apply them to the svg
+# update the SVG with the invitees ----------------
+
 for table_n in list(map(str, range(1,4))):
     for side in ['L', 'R']:
-        table_in_svg = tables_in_svg[table_n][side]
-        table_to_apply = tables_to_apply[table_n][side]
+        seats_in_svg = tables_in_svg[table_n][side].find_all('text')
+        seats_to_apply = tables_to_apply[table_n][side]
 
-        for seat_in_svg, seat_to_apply in zip(table_in_svg.find_all('text'), table_to_apply):
-            print(f'{seat_in_svg=} {seat_to_apply=}')
-            text_in_svg = seat_in_svg.find('tspan')
-            text_in_svg.string = seat_to_apply
-            print(f'{seat_in_svg=}\n')
+        for seat_in_svg, seat_to_apply in zip(seats_in_svg, seats_to_apply):
+            seat_in_svg.find('tspan').string = seat_to_apply
+            print(f'{seat_to_apply=} > {seat_in_svg=}\n')
 
-# print(soup.prettify(), file=open('steating_modified.svg', 'w'))
-print(soup, file=open(ofpath, 'w'))
+print(soup, file=open(svg_ofpath, 'w'))
+
+# add the SVG to the HTML page ------------------
+
+print(f'writing to {html_ofpath=}')
+soup_html = BeautifulSoup(open(html_ofpath).read(), 'html.parser')
+soup_html.body.clear()
+
+soup_html.body.append(
+    soup_html.new_tag(
+        'object',
+        type  = 'image/svg+xml',
+        data  = 'seating.svg',
+        style = 'margin: auto; display: block; width: 70%; padding-top: 5%;',
+        img=soup_html.new_tag(
+            'img',
+            src = 'seating.svg',
+            alt = 'Seating Chart'
+        ),
+    )
+)
+
+print(soup_html.prettify(), file=open(html_ofpath, 'w'))
