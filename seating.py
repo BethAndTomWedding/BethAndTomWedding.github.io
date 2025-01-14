aa7295f665da8f9b9f2f0fb08abf5dc1627722be
Tom McKeesick <tmck01@gmail.com>
2025-01-14 20:19:31 +1100

    update the converter

--- seating.py
+++ seating.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-import csv, json
+import csv, json, sys
 from bs4 import BeautifulSoup
+
+ifpath, ofpath = sys.argv[1], sys.argv[2]
+
+print(f'{ifpath=} > {ofpath=}')
 
 tables_to_apply = {'1': {'L': [], 'R': []}, '2': {'L': [], 'R': []},'3': {'L': [], 'R': []},}
 with open('invitees.csv') as istream:
@@ -14,34 +18,38 @@ with open('invitees.csv') as istream:
             tables_to_apply[table_n][side].append(person_name)
 print(json.dumps(tables_to_apply, indent=2))
 
-soup = BeautifulSoup(open('seating3.svg').read(), features='xml')
-groups = soup.find('g', {'id': 'layer1'}).find('g')
+soup = BeautifulSoup(open(ifpath).read(), features='xml')
 
-ts = groups.find_all('g')
+print(soup.prettify())
 
-t = {
-    '1': ts[0],
-    '2': ts[1],
-    '3': ts[2],
+tables_in_svg = {
+    '1': {
+        'L': soup.find('g', {'id': 'table1LHS'}),
+        'R': soup.find('g', {'id': 'table1RHS'}),
+    },
+    '2': {
+        'L': soup.find('g', {'id': 'table2LHS'}),
+        'R': soup.find('g', {'id': 'table2RHS'}),
+    },
+    '3': {
+        'L': soup.find('g', {'id': 'table3LHS'}),
+        'R': soup.find('g', {'id': 'table3RHS'}),
+    },
 }
 
-n_seats = 13
-for table_n, table_group in t.items():
-    side, idx = 'L', 0
-    for seat_n, seat in enumerate(table_group.findChildren('text')):
-        if idx >= len(tables_to_apply[table_n][side])-1:
-            table_len = len(tables_to_apply[table_n][side])-1
-            print(f'{side=}, idx > {table_len=}, setting to empty inc {idx=} > {idx+1}')
-            seat.string = ''
-        else:
-            seat.string = tables_to_apply[table_n][side][idx]
-            print('<< ', side, seat_n, idx, seat.string)
+print(tables_in_svg)
 
-        idx += 1
+# take the seats from the "tables_to_apply" and apply them to the svg
+for table_n in list(map(str, range(1,4))):
+    for side in ['L', 'R']:
+        table_in_svg = tables_in_svg[table_n][side]
+        table_to_apply = tables_to_apply[table_n][side]
 
-        if idx == n_seats-1 and side == 'L':
-            print(side, idx, '', 'changing side to R')
-            idx = 0
-            side = 'R'
+        for seat_in_svg, seat_to_apply in zip(table_in_svg.find_all('text'), table_to_apply):
+            print(f'{seat_in_svg=} {seat_to_apply=}')
+            text_in_svg = seat_in_svg.find('tspan')
+            text_in_svg.string = seat_to_apply
+            print(f'{seat_in_svg=}\n')
 
-print(soup.prettify(), file=open('steating_modified.svg', 'w'))
+# print(soup.prettify(), file=open('steating_modified.svg', 'w'))
+print(soup, file=open(ofpath, 'w'))
